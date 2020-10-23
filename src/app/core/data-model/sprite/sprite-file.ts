abcacdafe3b3b0db87ae866866cbb7603c5c7818
YI ZHANG <scaccterran@gmail.com>
2020-10-23 11:37:05 -0400

    CGM-12 use image display panel

--- src/app/core/data-model/sprite/sprite-file.ts
+++ src/app/core/data-model/sprite/sprite-file.ts
@@ -1,3 +1,7 @@
 export class SpriteFile {
+    public name: string;
 
+    constructor(name: string) {
+        this.name = name;
+    }
 }
--- src/app/features/image-sprite/sprite-previewer/sprite-previewer.component.ts
+++ src/app/features/image-sprite/sprite-previewer/sprite-previewer.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { FileSystemFileEntry } from 'ngx-file-drop';
 
 @Component({
@@ -13,23 +12,4 @@ export class SpritePreviewerComponent implements OnInit {
     @Input() public confirmAction = 'Confirm';
     @Output() public confirm = new EventEmitter();
     @Output() public cancel = new EventEmitter();
-    private _objectUrl = '';
-    private _safeUrl: SafeUrl;
-
-    constructor(private _sanitizer: DomSanitizer) { }
-
-    get safeUrl(): SafeUrl {
-        return this._safeUrl;
-    }
-
-    public ngOnInit(): void {
-        this.file.file(_ => {
-            this._objectUrl = URL.createObjectURL(_ as Blob);
-            this._safeUrl = this._sanitizer.bypassSecurityTrustUrl(this._objectUrl);
-        });
-    }
-
-    public onImageLoaded(): void {
-        URL.revokeObjectURL(this._objectUrl);
-    }
 }
