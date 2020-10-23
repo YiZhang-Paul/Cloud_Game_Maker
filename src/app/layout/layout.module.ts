d5c27473aaa4523da5e924b29eb8643ed7973ddb
YI ZHANG <scaccterran@gmail.com>
2020-10-22 21:24:38 -0400

    CGM-2 top toolbar component

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
+
+@NgModule({
+    declarations: [TopToolbarComponent],
+    imports: [CommonModule],
+    exports: [TopToolbarComponent]
+})
+export class LayoutModule { }
