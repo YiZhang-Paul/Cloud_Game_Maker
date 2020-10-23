23994ca2ee5dd4e7677b424e9e961d0d08c7f42c
YI ZHANG <scaccterran@gmail.com>
2020-10-22 21:43:17 -0400

    CGM-2 use fxFlex module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 
 @NgModule({
     declarations: [TopToolbarComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        FlexLayoutModule
+    ],
     exports: [TopToolbarComponent]
 })
 export class LayoutModule { }
