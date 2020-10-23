c45ded0d4acff6be3da509a6a72679f19bbca319
YI ZHANG <scaccterran@gmail.com>
2020-10-23 01:27:23 -0400

    CGM-12 added button to import sprite

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
-        FlexLayoutModule
+        FlexLayoutModule,
+        NgxFileDropModule
     ],
-    exports: [FlexLayoutModule]
+    exports: [
+        FlexLayoutModule,
+        NgxFileDropModule
+    ]
 })
 export class SharedModule { }
