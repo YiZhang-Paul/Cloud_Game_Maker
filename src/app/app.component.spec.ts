445a5ae34cfe3ce6e5f88e0d0441d99f946dc7a7
YI ZHANG <scaccterran@gmail.com>
2020-10-22 20:00:54 -0400

    CGM-1 setup Jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('app component unit test', () => {
+    let fixture: ComponentFixture<AppComponent>;
+    let component: AppComponent;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [
@@ -12,24 +16,12 @@ describe('AppComponent', () => {
                 AppComponent
             ],
         }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
     });
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
-    });
-
-    it(`should have as title 'game-maker'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('game-maker');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('game-maker app is running!');
+    test('should create the component', () => {
+        expect(component).toBeTruthy();
     });
 });
