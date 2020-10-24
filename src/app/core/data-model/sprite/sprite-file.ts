cfaac5427f88b5f18b1fb39067853ec7f5dac221
YI ZHANG <scaccterran@gmail.com>
2020-10-24 16:09:11 -0400

    CGM-12 prevent editing extension

--- src/app/core/data-model/sprite/sprite-file.ts
+++ src/app/core/data-model/sprite/sprite-file.ts
@@ -5,6 +5,7 @@ import { FileUtility } from '../../utility/file.utility';
 export class SpriteFile {
     public name: string;
     public type: string;
+    public extension: string;
     public base64: string;
 
     get imageSrc(): string {
@@ -17,8 +18,9 @@ export class SpriteFile {
         }
 
         const sprite = new SpriteFile();
-        sprite.name = name;
+        sprite.name = name.replace(/\.[^.]*$/g, '');
         sprite.type = base64.split(',')[0].replace(/data:|;base64/g, '');
+        sprite.extension = sprite.type.includes('png') ? 'png' : 'jpg';
         sprite.base64 = base64.split(',')[1];
 
         return sprite;
@@ -27,8 +29,9 @@ export class SpriteFile {
     public static async fromFileEntry(file: FileSystemFileEntry): Promise<SpriteFile> {
         const blob: Blob = await new Promise(resolve => file.file(resolve));
         const sprite = new SpriteFile();
-        sprite.name = file.name;
+        sprite.name = file.name.replace(/\.[^.]*$/g, '');
         sprite.type = blob.type;
+        sprite.extension = sprite.type.includes('png') ? 'png' : 'jpg';
         sprite.base64 = await FileUtility.toBase64(blob);
 
         return sprite;
