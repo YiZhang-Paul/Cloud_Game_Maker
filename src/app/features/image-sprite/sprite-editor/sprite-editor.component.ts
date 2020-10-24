3d930c61c97be21782c24ff338e0098839f1690a
YI ZHANG <scaccterran@gmail.com>
2020-10-24 00:03:38 -0400

    CGM-12 added rotate and flip functionality

--- src/app/features/image-sprite/sprite-editor/sprite-editor.component.ts
+++ src/app/features/image-sprite/sprite-editor/sprite-editor.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { ImageCroppedEvent, ImageTransform } from 'ngx-image-cropper';
 
 import { SpriteFile } from '../../../core/data-model/sprite/sprite-file';
 import { SpriteEditorMode } from '../../../core/enum/sprite-editor-mode.enum';
@@ -16,14 +16,34 @@ export class SpriteEditorComponent {
     public mode = SpriteEditorMode.Readonly;
     public modes = SpriteEditorMode;
     private _targetImage: Blob;
+    private _imageTransform: ImageTransform = { scale: 1, rotate: 0, flipH: false, flipV: false };
 
     get targetImage(): Blob {
         return this._targetImage;
     }
 
+    get imageTransform(): ImageTransform {
+        return this._imageTransform;
+    }
+
     public onImageCropStart(): void {
         this.mode = this.modes.Crop;
         this.file.raw.file(_ => this._targetImage = _);
+    }
+
+    public onImageRotate(): void {
+        const rotate = (this._imageTransform.rotate + 90) % 360;
+        this._imageTransform = { ...this._imageTransform, rotate };
+    }
+
+    public onImageFlip(isVertical = false): void {
+        const { flipH, flipV } = this._imageTransform;
+
+        this._imageTransform = {
+            ...this._imageTransform,
+            flipH: isVertical ? flipH : !flipH,
+            flipV: isVertical ? !flipV : flipV
+        };
     }
 
     public onImageCropped(event: ImageCroppedEvent): void {
