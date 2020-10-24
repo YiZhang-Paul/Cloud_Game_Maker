4f8e8d5bd2ad2c61b6374ea31988edc1c198747d
YI ZHANG <scaccterran@gmail.com>
2020-10-24 13:00:11 -0400

    CGM-12 only handle base64

--- src/app/shared/components/panels/image-display-panel/image-display-panel.component.ts
+++ src/app/shared/components/panels/image-display-panel/image-display-panel.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { FileSystemFileEntry } from 'ngx-file-drop';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { SpriteFile } from '../../../../core/data-model/sprite/sprite-file';
 
 @Component({
     selector: 'app-image-display-panel',
@@ -8,25 +8,6 @@ import { FileSystemFileEntry } from 'ngx-file-drop';
     styleUrls: ['./image-display-panel.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ImageDisplayPanelComponent implements OnInit {
-    @Input() public file: FileSystemFileEntry;
-    private _objectUrl = '';
-    private _safeUrl: SafeUrl;
-
-    constructor(private _sanitizer: DomSanitizer) { }
-
-    get safeUrl(): SafeUrl {
-        return this._safeUrl;
-    }
-
-    public ngOnInit(): void {
-        this.file.file(_ => {
-            this._objectUrl = URL.createObjectURL(_ as Blob);
-            this._safeUrl = this._sanitizer.bypassSecurityTrustUrl(this._objectUrl);
-        });
-    }
-
-    public onLoad(): void {
-        URL.revokeObjectURL(this._objectUrl);
-    }
+export class ImageDisplayPanelComponent {
+    @Input() public file: SpriteFile;
 }
