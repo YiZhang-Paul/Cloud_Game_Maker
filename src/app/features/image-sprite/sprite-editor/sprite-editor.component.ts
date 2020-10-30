259549fe5ab4eff4dd70522de037f8e6e0c47def
YI ZHANG <scaccterran@gmail.com>
2020-10-30 02:48:20 -0400

    CGM-12 check aspect ratio before saving image

--- src/app/features/image-sprite/sprite-editor/sprite-editor.component.ts
+++ src/app/features/image-sprite/sprite-editor/sprite-editor.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ImageCropperComponent, ImageTransform } from 'ngx-image-cropper';
+import { Dimensions, ImageCropperComponent, ImageTransform } from 'ngx-image-cropper';
 
 import { SpriteFile } from '../../../core/data-model/sprite/sprite-file';
 import { ConfirmPopupOption } from '../../../core/data-model/generic/options/confirm-popup-option';
@@ -23,6 +23,7 @@ export class SpriteEditorComponent implements OnInit {
     @ViewChild('cropper') private _cropper: ImageCropperComponent;
     public isPreviewing = false;
     private _isCropperReady = false;
+    private _isCorrectRatio = false;
     private _transform: ImageTransform;
     private _modifiedFile: SpriteFile;
 
@@ -62,8 +63,9 @@ export class SpriteEditorComponent implements OnInit {
         this._modifiedFile.name = name;
     }
 
-    public onCropperReady(): void {
+    public onCropperReady({ width, height }: Dimensions): void {
         this._isCropperReady = true;
+        this._isCorrectRatio = width === height;
     }
 
     public onImageRotate(): void {
@@ -90,7 +92,8 @@ export class SpriteEditorComponent implements OnInit {
             this._modifiedFile = SpriteFile.fromSpriteFile(this.file);
         }
 
-        const { base64 } = this._cropper.crop();
+        const { base64, width, height } = this._cropper.crop();
+        this._isCorrectRatio = width === height;
         this._modifiedFile.mime = 'image/jpeg';
         this._modifiedFile.extension = 'jpg';
         this._modifiedFile.content = FileUtility.base64ToBlob(base64, this._modifiedFile.mime);
@@ -102,7 +105,30 @@ export class SpriteEditorComponent implements OnInit {
         this._transform = { scale: 1, rotate: 0, flipH: false, flipV: false };
     }
 
-    public async onImageSave(): Promise<void> {
+    public async onImageSave(isNew = true): Promise<void> {
+        if (!this._isCorrectRatio) {
+            const title = 'Invalid aspect ratio';
+            const message = 'Please crop the image to ensure 1:1 ratio.';
+            const actions = [new ConfirmActionOption('Got It', null)];
+
+            this._dialog.open(ConfirmPopupComponent, {
+                data: new ConfirmPopupOption(title, message, actions),
+                width: '350px',
+                height: '175px'
+            });
+
+            return;
+        }
+
+        if (isNew) {
+            this.importNew.emit(this.targetFile);
+        }
+        else {
+            await this.saveExistingImage();
+        }
+    }
+
+    public async saveExistingImage(): Promise<void> {
         const title = 'Overwrite existing sprite?';
         const message = 'You can save the changes as a new sprite.';
         const option = ['Overwrite', 'Save as New', 'Cancel'];
