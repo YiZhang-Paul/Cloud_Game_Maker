046015cec7dd7a4b6d57705ccc1d32bf8c79a50e
YI ZHANG <scaccterran@gmail.com>
2020-10-29 23:59:44 -0400

    CGM-12 moved previewer to new folder

--- src/app/features/image-sprite/image-sprite.module.ts
+++ src/app/features/image-sprite/image-sprite.module.ts
@@ -4,17 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 
 import { SpriteManagerComponent } from './sprite-manager/sprite-manager.component';
-import { SpritePreviewerComponent } from './sprite-previewer/sprite-previewer.component';
 import { SpriteThumbnailItemComponent } from './sprite-manager/sprite-thumbnail-item/sprite-thumbnail-item.component';
 import { SpriteManagerToolbarComponent } from './sprite-manager/sprite-manager-toolbar/sprite-manager-toolbar.component';
+import { SpritePreviewerComponent } from './sprite-editor/sprite-previewer/sprite-previewer.component';
 import { SpriteEditorComponent } from './sprite-editor/sprite-editor.component';
 
 @NgModule({
     declarations: [
         SpriteManagerComponent,
-        SpritePreviewerComponent,
         SpriteThumbnailItemComponent,
         SpriteManagerToolbarComponent,
+        SpritePreviewerComponent,
         SpriteEditorComponent
     ],
     imports: [
