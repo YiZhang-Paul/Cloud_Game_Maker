3ef2dca8c7335df2c0e27e96464636f9132b9f8c
YI ZHANG <scaccterran@gmail.com>
2020-10-30 01:06:02 -0400

    CGM-12 display as url when blob is not available

--- src/app/features/image-sprite/sprite-editor/sprite-previewer/sprite-previewer.component.ts
+++ src/app/features/image-sprite/sprite-editor/sprite-previewer/sprite-previewer.component.ts
@@ -10,7 +10,5 @@ import { SpriteFile } from '../../../../core/data-model/sprite/sprite-file';
 })
 export class SpritePreviewerComponent {
     @Input() public file: SpriteFile;
-    @Input() public confirmAction = 'Confirm';
-    @Output() public confirm = new EventEmitter();
     @Output() public cancel = new EventEmitter();
 }
