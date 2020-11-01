04d50a93b8de1efb2722964350cbd20a7d7d5f1c
YI ZHANG <scaccterran@gmail.com>
2020-10-31 22:41:54 -0400

    CGM-22 add toolbar to scene manager

--- src/app/features/game-scene/scene-manager/scene-manager.component.ts
+++ src/app/features/game-scene/scene-manager/scene-manager.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Scene } from '../../../core/data-model/scene/scene';
+import { MiniToolbarOption } from '../../../core/enum/mini-toolbar-option.enum';
 
 @Component({
     selector: 'app-scene-manager',
@@ -8,13 +9,25 @@ import { Scene } from '../../../core/data-model/scene/scene';
     styleUrls: ['./scene-manager.component.scss']
 })
 export class SceneManagerComponent {
+    public toolbarOptions = [MiniToolbarOption.Create, MiniToolbarOption.Search];
     private _scenes: Scene[] = [];
+    private _filteredScenes: Scene[] = [];
 
     get scenes(): Scene[] {
         return this._scenes;
     }
 
+    get filteredScenes(): Scene[] {
+        return this._filteredScenes;
+    }
+
     public onSceneCreate(): void {
         this._scenes.push(new Scene());
+        this._filteredScenes = this._scenes.slice();
+    }
+
+    public onFileSearch(keyword: string): void {
+        const lowercase = keyword.toLowerCase();
+        this._filteredScenes = this._scenes.filter(_ => _.name.toLowerCase().includes(lowercase));
     }
 }
