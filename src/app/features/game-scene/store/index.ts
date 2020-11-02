a806f8cd398074d6e4a297def54526d085f28d55
YI ZHANG <scaccterran@gmail.com>
2020-11-01 23:37:15 -0500

    CGM-14 setup and added effects

--- src/app/features/game-scene/store/index.ts
+++ src/app/features/game-scene/store/index.ts
@@ -1,7 +1,11 @@
 import * as state from './game-scene.state';
 import * as actions from './game-scene.actions';
+import * as reducers from './game-scene.reducers';
+import * as effects from './game-scene.effects';
 
 export default {
     state,
-    actions
+    actions,
+    reducers,
+    effects
 };
