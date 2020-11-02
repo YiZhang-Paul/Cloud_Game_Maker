fb333033c501cda4e09756a9ea9e3762fe15eb6b
YI ZHANG <scaccterran@gmail.com>
2020-11-01 23:34:30 -0500

    CGM-14 added additional actions

--- src/app/features/game-scene/store/game-scene.actions.ts
+++ src/app/features/game-scene/store/game-scene.actions.ts
@@ -2,5 +2,7 @@ import { createAction, props } from '@ngrx/store';
 
 import { Scene } from '../../../core/data-model/scene/scene';
 
+export const loadScenesRemote = createAction('[Scene Manager] Load Scenes Remote');
+export const addScenes = createAction('[Scene Manager] Add Scenes', props<{ payload: Scene[] }>());
 export const addScene = createAction('[Scene Manager] Add Scene', props<Scene>());
 export const deleteScene = createAction('[Scene Manager] Delete Scene', props<Scene>());
