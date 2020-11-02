3cfc322c3d384a34ece3b0985323aa384ea9edc1
YI ZHANG <scaccterran@gmail.com>
2020-11-01 23:36:15 -0500

    CGM-14 minor refactor

--- src/app/features/game-scene/store/game-scene.actions.ts
+++ src/app/features/game-scene/store/game-scene.actions.ts
@@ -2,7 +2,9 @@ import { createAction, props } from '@ngrx/store';
 
 import { Scene } from '../../../core/data-model/scene/scene';
 
-export const loadScenesRemote = createAction('[Scene Manager] Load Scenes Remote');
-export const addScenes = createAction('[Scene Manager] Add Scenes', props<{ payload: Scene[] }>());
-export const addScene = createAction('[Scene Manager] Add Scene', props<Scene>());
-export const deleteScene = createAction('[Scene Manager] Delete Scene', props<Scene>());
+const source = '[Scene Manager]';
+
+export const loadScenesRemote = createAction(`${source} Load Scenes Remote`);
+export const addScenes = createAction(`${source} Add Scenes`, props<{ payload: Scene[] }>());
+export const addScene = createAction(`${source} Add Scene`, props<Scene>());
+export const deleteScene = createAction(`${source} Delete Scene`, props<Scene>());
