9a4411e805ced70779bb2b044e46e648f36d6d10
YI ZHANG <scaccterran@gmail.com>
2020-11-02 01:30:53 -0500

    CGM-14 simplified state

--- src/app/features/game-scene/store/game-scene.selectors.ts
+++ src/app/features/game-scene/store/game-scene.selectors.ts
@@ -1,13 +1,7 @@
-import { createSelector } from '@ngrx/store';
-
-import { Scene } from '../../../core/data-model/scene/scene';
-
 import { IGameSceneState } from './game-scene.state';
 
 export const selectTotalScenes = (state: IGameSceneState) => state.scenes.length;
 
-export const selectFilteredScenes = createSelector(
-    (state: IGameSceneState) => state.scenes,
-    (state: IGameSceneState) => state.sceneFilter,
-    (scenes: Scene[], filter: string) => scenes.filter(_ => _.name.toLowerCase().includes(filter))
-);
+export const selectFilteredScenes = (state: IGameSceneState, filter: string) => {
+    return state.scenes.filter(_ => _.name.toLowerCase().includes(filter));
+};
--- src/app/features/game-scene/store/game-scene.state.ts
+++ src/app/features/game-scene/store/game-scene.state.ts
@@ -2,10 +2,8 @@ import { Scene } from '../../../core/data-model/scene/scene';
 
 export interface IGameSceneState {
     scenes: Scene[];
-    sceneFilter: string;
 }
 
 export const initialState: IGameSceneState = {
-    scenes: [],
-    sceneFilter: ''
+    scenes: []
 };
