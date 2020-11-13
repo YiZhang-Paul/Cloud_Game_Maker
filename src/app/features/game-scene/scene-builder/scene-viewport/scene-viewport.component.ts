dc39928635b64efd5347e8888863ae57710cab48
YI ZHANG <scaccterran@gmail.com>
2020-11-12 23:04:21 -0500

    CGM-6 allow dragging camera

--- src/app/features/game-scene/scene-builder/scene-viewport/scene-viewport.component.ts
+++ src/app/features/game-scene/scene-builder/scene-viewport/scene-viewport.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 
 import { Scene } from '../../../../core/data-model/scene/scene';
 
@@ -11,14 +11,61 @@ import { Scene } from '../../../../core/data-model/scene/scene';
 export class SceneViewportComponent {
     @Input() public scene: Scene;
     @ViewChild('viewport') private _viewport: ElementRef;
-    private _x = 0;
-    private _y = 0;
+    private _hasFocus = true;
+    private _canDragPointer = false;
+    private _canMoveCamera = false;
+    private _pointerX = 0;
+    private _pointerY = 0;
+    private _deltaX = 0;
+    private _deltaY = 0;
+    private _viewportX = 0;
+    private _viewportY = 0;
 
-    get x(): number {
-        return this._x;
+    get viewportStyle(): { [key: string]: boolean } {
+        return {
+            draggable: this._canDragPointer,
+            moveable: this._canMoveCamera
+        };
     }
 
-    get y(): number {
-        return this._y;
+    @HostListener('document:keydown', ['$event'])
+    public onDocumentKeydown(event: KeyboardEvent): void {
+        if (this._hasFocus && event.code === 'Space') {
+            this._canDragPointer = true;
+        }
+    }
+
+    @HostListener('document:keyup', ['$event'])
+    public onDocumentKeyup(event: KeyboardEvent): void {
+        if (event.code === 'Space') {
+            this._canDragPointer = false;
+        }
+    }
+
+    @HostListener('document:mousedown', ['$event'])
+    public onDocumentMousedown(event: MouseEvent): void {
+        if (this._canDragPointer) {
+            this._canMoveCamera = true;
+            [this._pointerX, this._pointerY] = [event.clientX, event.clientY];
+        }
+        else {
+            this._hasFocus = this._viewport?.nativeElement?.contains(event.target);
+        }
+    }
+
+    @HostListener('document:mouseup')
+    public onDocumentMouseup(): void {
+        this._canMoveCamera = false;
+        this._viewportX += this._deltaX;
+        this._viewportY += this._deltaY;
+        [this._deltaX, this._deltaY] = [0, 0];
+    }
+
+    @HostListener('document:mousemove', ['$event'])
+    public onDocumentMousemove(event: MouseEvent): void {
+        if (this._canMoveCamera) {
+            this._deltaX = event.clientX - this._pointerX;
+            this._deltaY = event.clientY - this._pointerY;
+        }
     }
 }
