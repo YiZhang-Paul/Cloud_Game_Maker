1602a9169f4f8b358c13f7ff55e095b9d7e64c15
YI ZHANG <scaccterran@gmail.com>
2020-11-15 17:52:28 -0500

    CGM-13 use camera to handle most of viewport rendering

--- src/app/features/game-scene/scene-builder/scene-viewport/scene-viewport.component.ts
+++ src/app/features/game-scene/scene-builder/scene-viewport/scene-viewport.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
 
-import { Point } from '../../../../core/data-model/generic/point';
 import { Scene } from '../../../../core/data-model/scene/scene';
-import { GenericUtility } from '../../../../core/utility/generic-utility/generic.utility';
+import { Point } from '../../../../../engine/core/data-model/generic/point';
+import { Camera } from '../../../../../engine/rendering/camera/camera';
 
 @Component({
     selector: 'app-scene-viewport',
@@ -14,29 +14,11 @@ export class SceneViewportComponent implements AfterViewInit {
     @Input() public scene: Scene;
     @Output() public sceneChange = new EventEmitter<Scene>();
     @ViewChild('viewport') private _viewport: ElementRef;
-    public columns: number[] = [];
-    public rows: number[] = [];
     private _hasFocus = true;
     private _canDragPointer = false;
     private _canMoveCamera = false;
     private _pointerXY = new Point();
-    private _deltaXY = new Point();
-
-    get viewportX(): number {
-        const { layers, scale, viewportXY } = this.scene;
-        const totalWidth = layers[0].grids[0].length * scale;
-        const maxX = totalWidth - this._viewport?.nativeElement?.clientWidth ?? 0;
-
-        return GenericUtility.limitValue(viewportXY.x + this._deltaXY.x, 0, maxX);
-    }
-
-    get viewportY(): number {
-        const { layers, scale, viewportXY } = this.scene;
-        const totalHeight = layers[0].grids.length * scale;
-        const maxY = totalHeight - this._viewport?.nativeElement?.clientHeight ?? 0;
-
-        return GenericUtility.limitValue(viewportXY.y + this._deltaXY.y, 0, maxY);
-    }
+    private _camera: Camera;
 
     get viewportClass(): { [key: string]: boolean } {
         return {
@@ -46,26 +28,35 @@ export class SceneViewportComponent implements AfterViewInit {
     }
 
     get layerStyle(): { [key: string]: string } {
-        return {
-            top: `${-this.viewportY % this.scene.scale}px`,
-            left: `${-this.viewportX % this.scene.scale}px`,
-            width: `${this.columns.length * this.scene.scale}px`,
-            height: `${this.rows.length * this.scene.scale}px`
-        };
+        if (this._camera) {
+            return {
+                top: `${-this._camera.offsetY}px`,
+                left: `${-this._camera.offsetX}px`,
+                width: `${this._camera.renderWidth}px`,
+                height: `${this._camera.renderHeight}px`
+            };
+        }
     }
 
     public ngAfterViewInit(): void {
-        setTimeout(() => this.renderLayer());
+        setTimeout(() => {
+            const { clientWidth: width, clientHeight: height } = this._viewport.nativeElement;
+            const { scale, layers, viewportXY } = this.scene;
+            const position = new Point(viewportXY.x, viewportXY.y);
+            const rows = layers[0].grids.length;
+            const columns = layers[0].grids[0].length;
+            this._camera = new Camera(width, height, position, rows, columns, scale);
+            this.renderViewport();
+        });
     }
 
     @HostListener('document:wheel', ['$event'])
     public onDocumentScroll(event: WheelEvent): void {
         if (this._viewport?.nativeElement?.contains(event.target)) {
-            const delta = event.deltaY > 0 ? 10 : -10;
-            const scale = GenericUtility.limitValue(this.scene.scale + delta, 30, 200);
-            this.scene = { ...this.scene, scale };
+            this._camera.changeScale(event.deltaY > 0 ? 10 : -10);
+            this.scene = { ...this.scene, scale: this._camera.scale };
             this.sceneChange.emit(this.scene);
-            this.renderLayer();
+            this.renderViewport();
         }
     }
 
@@ -79,8 +70,8 @@ export class SceneViewportComponent implements AfterViewInit {
     @HostListener('document:keyup', ['$event'])
     public onDocumentKeyup(event: KeyboardEvent): void {
         if (event.code === 'Space') {
-            this._canMoveCamera = false;
             this._canDragPointer = false;
+            this._canMoveCamera = false;
         }
     }
 
@@ -103,28 +94,24 @@ export class SceneViewportComponent implements AfterViewInit {
     @HostListener('document:mousemove', ['$event'])
     public onDocumentMousemove({ clientX, clientY }: MouseEvent): void {
         if (this._canMoveCamera) {
-            this._deltaXY = new Point(this._pointerXY.x - clientX, this._pointerXY.y - clientY);
-            this.renderLayer();
+            this._camera.move(this._pointerXY.x - clientX, this._pointerXY.y - clientY);
+            this._pointerXY = new Point(clientX, clientY);
+            this.renderViewport();
         }
     }
 
     @HostListener('document:mouseup')
     public onDocumentMouseup(): void {
         if (this._canMoveCamera) {
-            const viewportXY = new Point(this.viewportX, this.viewportY);
-            this.scene = { ...this.scene, viewportXY };
+            const { x, y } = this._camera.position;
+            this.scene = { ...this.scene, viewportXY: new Point(x, y) };
             this.sceneChange.emit(this.scene);
-            this._deltaXY = new Point(0, 0);
             this._canMoveCamera = false;
         }
     }
 
-    private renderLayer(): void {
-        if (!this._viewport) {
-            return;
-        }
-
-        this.setViewportArea();
+    private renderViewport(): void {
+        this._camera.setRenderArea();
 
         for (let i = 0; i < this.scene.layers.length; ++i) {
             this.drawViewport(i);
@@ -133,37 +120,26 @@ export class SceneViewportComponent implements AfterViewInit {
         this.drawGridLines();
     }
 
-    private setViewportArea(): void {
-        const { clientWidth, clientHeight } = this._viewport.nativeElement;
-        const startColumn = Math.floor(this.viewportX / this.scene.scale);
-        const startRow = Math.floor(this.viewportY / this.scene.scale);
-        const endColumn = Math.floor((this.viewportX + clientWidth) / this.scene.scale);
-        const endRow = Math.floor((this.viewportY + clientHeight) / this.scene.scale);
-        this.columns = GenericUtility.getValueRange(startColumn, endColumn);
-        this.rows = GenericUtility.getValueRange(startRow, endRow);
-    }
-
     private drawViewport(index: number): void { }
 
     private drawGridLines(): void {
         const canvas = document.getElementById('grid-line-layer') as HTMLCanvasElement;
         const context = canvas.getContext('2d');
-        const gridWidth = this.scene.scale;
-        canvas.width = this.columns.length * gridWidth;
-        canvas.height = this.rows.length * gridWidth;
+        canvas.width = this._camera.renderWidth;
+        canvas.height = this._camera.renderHeight;
         context.clearRect(0, 0, canvas.width, canvas.height);
-        context.strokeStyle = 'lime';
+        context.strokeStyle = 'rgb(0, 255, 0)';
 
-        for (const column of this.columns) {
-            const x = (column - this.columns[0]) * gridWidth;
+        for (let i = 0; i < this._camera.renderColumns; ++i) {
+            const x = i * this._camera.scale;
             context.beginPath();
             context.moveTo(x, 0);
             context.lineTo(x, canvas.height);
             context.stroke();
         }
 
-        for (const row of this.rows) {
-            const y = (row - this.rows[0]) * gridWidth;
+        for (let i = 0; i < this._camera.renderRows; ++i) {
+            const y = i * this._camera.scale;
             context.beginPath();
             context.moveTo(0, y);
             context.lineTo(canvas.width, y);
