cbbe1380c44c38affccc1ac02e72eadb7599933f
YI ZHANG <scaccterran@gmail.com>
2020-11-15 19:10:07 -0500

    CGM-23 allow editing name from thumbnail

--- src/app/features/image-sprite/sprite-manager/sprite-thumbnail-item/sprite-thumbnail-item.component.ts
+++ src/app/features/image-sprite/sprite-manager/sprite-thumbnail-item/sprite-thumbnail-item.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 import { SpriteFile } from '../../../../core/data-model/sprite/sprite-file';
 
@@ -12,4 +12,22 @@ export class SpriteThumbnailItemComponent {
     @Input() public file: SpriteFile;
     @Output() public editStart = new EventEmitter();
     @Output() public delete = new EventEmitter();
+    @Output() public nameChange = new EventEmitter<string>();
+    @ViewChild('nameInput') private _nameInput: ElementRef;
+    private _isEditingName = false;
+
+    get isEditingName(): boolean {
+        return this._isEditingName;
+    }
+
+    public toggleNameEdit(): void {
+        this._isEditingName = !this._isEditingName;
+
+        if (!this._isEditingName) {
+            this.nameChange.emit(this._nameInput.nativeElement.value);
+        }
+        else {
+            setTimeout(() => this._nameInput.nativeElement.focus());
+        }
+    }
 }
