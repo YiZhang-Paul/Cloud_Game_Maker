f35a2cbe579d85758e6165904e3f3a41dc93fca2
YI ZHANG <scaccterran@gmail.com>
2020-11-22 13:26:09 -0500

    CGM-9 fixed compiler error

--- src/app/features/game-scene/scene-builder/scene-builder.component.ts
+++ src/app/features/game-scene/scene-builder/scene-builder.component.ts
@@ -18,7 +18,7 @@ export class SceneBuilderComponent implements OnInit {
     public activeScene$: Observable<Scene>;
     public openedScenes$: Observable<Scene[]>;
     public draggedSprite$: Observable<SpriteFile>;
-    private _sceneChanges$ = new Subject<Scene>().pipe(debounceTime(5000));
+    private _sceneChanges$ = new Subject<Scene>();
 
     constructor(private _store: Store) { }
 
@@ -26,7 +26,10 @@ export class SceneBuilderComponent implements OnInit {
         this.activeScene$ = this._store.select(store.selectors.getActiveScene);
         this.openedScenes$ = this._store.select(store.selectors.getOpenedScenes);
         this.draggedSprite$ = this._store.select(globalStore.selectors.getDraggedSprite);
-        this._sceneChanges$.subscribe(scene => this._store.dispatch(store.actions.updateSceneRemote(scene)));
+
+        this._sceneChanges$.pipe(debounceTime(5000)).subscribe(scene => {
+            this._store.dispatch(store.actions.updateSceneRemote(scene));
+        });
     }
 
     public onSceneSelected(scene: Scene): void {
