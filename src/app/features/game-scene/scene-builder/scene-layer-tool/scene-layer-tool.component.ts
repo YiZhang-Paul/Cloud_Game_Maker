5d6884a3c7aa7e3b90dc5856a1868e0d76841e8d
YI ZHANG <scaccterran@gmail.com>
2020-11-24 00:18:54 -0500

    CGM-7 allow removing layer

--- src/app/features/game-scene/scene-builder/scene-layer-tool/scene-layer-tool.component.ts
+++ src/app/features/game-scene/scene-builder/scene-layer-tool/scene-layer-tool.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 import { SceneLayer } from '../../../../../engine/core/data-model/scene/scene-layer';
+import { ConfirmActionOption } from '../../../../core/data-model/options/confirm-action-option';
+import { ConfirmPopupOption } from '../../../../core/data-model/options/confirm-popup-option';
+import { ConfirmPopupComponent } from '../../../../shared/components/popups/confirm-popup/confirm-popup.component';
 import { FileUtility } from '../../../../core/utility/file-utility/file.utility';
 import { GenericUtility } from '../../../../core/utility/generic-utility/generic.utility';
 
@@ -14,6 +18,8 @@ export class SceneLayerToolComponent {
     @Input() public layers: SceneLayer[] = [];
     @Output() public layersChange = new EventEmitter<SceneLayer[]>();
 
+    constructor(private _dialog: MatDialog) { }
+
     public onLayerAdd(): void {
         const layer = new SceneLayer();
         const names = this.layers.map(_ => _.name);
@@ -22,6 +28,26 @@ export class SceneLayerToolComponent {
         layer.columns = this.layers[0].columns;
         this.layers = [...this.layers, layer];
         this.layersChange.emit(this.layers);
+    }
+
+    public onLayerDelete(layer: SceneLayer): void {
+        const title = 'Are you sure?';
+        const message = 'The layer will be removed permanently.';
+        const option = ['Proceed', 'Cancel'];
+        const actions = option.map((_, i) => new ConfirmActionOption(_, i));
+
+        const dialog = this._dialog.open(ConfirmPopupComponent, {
+            data: new ConfirmPopupOption(title, message, actions),
+            width: '350px',
+            height: '175px'
+        });
+
+        dialog.afterClosed().subscribe(result => {
+            if (result === actions[0].value) {
+                this.layers = this.layers.filter(_ => _.name !== layer.name);
+                this.layersChange.emit(this.layers);
+            }
+        });
     }
 
     public onVisibilityChange(value: boolean, layer: SceneLayer): void {
