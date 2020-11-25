02347786de8a4e95d5a22b4dba4e101083863989
YI ZHANG <scaccterran@gmail.com>
2020-11-24 23:27:17 -0500

    CGM-7 removed redundant code

--- src/app/features/image-sprite/sprite-manager/sprite-thumbnail-item/sprite-thumbnail-item.component.ts
+++ src/app/features/image-sprite/sprite-manager/sprite-thumbnail-item/sprite-thumbnail-item.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Point } from '../../../../../engine/core/data-model/generic/point';
 import { SpriteFile } from '../../../../../engine/core/data-model/sprite/sprite-file';
@@ -17,17 +17,7 @@ export class SpriteThumbnailItemComponent {
     @Output() public nameChange = new EventEmitter<string>();
     @Output() public dragBegin = new EventEmitter();
     @Output() public dragCancel = new EventEmitter();
-    @ViewChild('nameInput') private _nameInput: ElementRef;
-    private _isEditingName = false;
     private _holdTimer: number;
-
-    get isEditingName(): boolean {
-        return this._isEditingName;
-    }
-
-    get editedName(): string {
-        return this._nameInput?.nativeElement?.value?.trim() ?? '';
-    }
 
     public onHoldStart(event: MouseEvent): void {
         this._holdTimer = window.setTimeout(() => {
@@ -49,24 +39,5 @@ export class SpriteThumbnailItemComponent {
 
     public ignoreDrag(event: MouseEvent): void {
         event.stopPropagation();
-    }
-
-    public toggleNameEdit(type: string): void {
-        if (this.isDragMode || type === 'blur' && !this._isEditingName) {
-            return;
-        }
-
-        this._isEditingName = !this._isEditingName;
-
-        if (!this._isEditingName && this.editedName !== this.file.name) {
-            this.nameChange.emit(this.editedName);
-        }
-        else {
-            setTimeout(() => {
-                if (this._isEditingName) {
-                    this._nameInput.nativeElement.focus();
-                }
-            });
-        }
     }
 }
